b4b8e5ec3ba8a01a977645ef9882c41b12cefa0a
Luis Fernando <fersdeveloper@gmail.com>
2022-01-31 18:52:20 -0600

    Practicing Property & Event Binding and View Encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {  }
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    firedNumber % 2 === 0
+      ? this.evenNumbers.push(firedNumber)
+      : this.oddNumbers.push(firedNumber);
+  }
+}
