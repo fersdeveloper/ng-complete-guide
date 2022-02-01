0370c9de7eadbd47c3e6ac1e9619d218ee810784
Luis Fernando <fersdeveloper@gmail.com>
2022-02-01 16:09:34 -0600

    Injecting Services intoServices

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import { Injectable } from "@angular/core";
+import { LogggingService } from "./logging/logging.service";
+
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +18,15 @@ export class AccountsService {
     },
   ];
 
+  constructor(private loggingService: LogggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({ name, status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
