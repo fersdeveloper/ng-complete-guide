0c55f8fb2d2d0b08aa74c8f85397cf09879e1f9d
Luis Fernando <fersdeveloper@gmail.com>
2022-02-01 17:13:39 -0600

    Optimize this app by adding a UsersService which manages the active and inactive users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {  }
+export class AppComponent {}
